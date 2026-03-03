bcd90906962d249c50982cd6d9b893eab2bb8c5a
joaopedroemvocacao <joaopedro11253131@gmail.com>
2026-03-03 17:00:34 -0300

    update upgrade reorganize rename
    Copia do sistema da minha hamburgueria, criamos um sistema com print, que contém o funcionamento do nosso menu

---
+++ Projeto_App_joaopedro.py
@@ -0,0 +1,15 @@
+  print('\n == Sistema De Hamburgueria == \')
+
+
+  print("1. Cadastro")
+  Print("2. Cardapio")
+  print("3. Solicitar Pedido")
+  print("4. Pagamento")
+  print("5. Acompanhar entrega")
+  print("6. Cancelar Entrega")
+  print("7. Sair")
+
+
+
+  while true:
+    escolha_menu = input("\n escolha uma opção")
